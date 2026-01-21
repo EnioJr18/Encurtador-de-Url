9cbd861710707ceb0f9d0ff79d9f8f72118773a3
Enio Jr <eniojr100@gmail.com>
2026-01-20 23:07:04 -0300

    :recycle: Refactor: Aumentando o tamanho da coluna short_code

--- models.py
+++ models.py
@@ -6,6 +6,6 @@ db = SQLAlchemy()
 class URL(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original_url = db.Column(db.String(500), nullable=False)
-    short_code = db.Column(db.String(6), unique=True, nullable=False)
+    short_code = db.Column(db.String(100), unique=True, nullable=False)
     click_count = db.Column(db.Integer, default=0)
     acesso_data = db.Column(db.DateTime)
