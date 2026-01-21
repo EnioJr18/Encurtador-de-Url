39625022dc5f3ca323d63e8d1ee7423a220b2a21
Enio Jr <eniojr100@gmail.com>
2026-01-20 22:10:31 -0300

    :wrench: Chore: Corrige requirements.txt removendo pywin32 para deploy no Linux

--- app.py
+++ app.py
@@ -4,23 +4,26 @@ from models import db
 from controllers import main
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'uma_senha_muito_dificil_e_secreta'
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'uma_senha_muito_dificil_e_secreta')
 
 
-uri = os.environ.get('DATABASE_URL', 'sqlite:///urls.db')
-if uri.startswith('postgres://'):
+uri = os.getenv('DATABASE_URL', 'sqlite:///urls.db')
+
+
+if uri and uri.startswith('postgres://'):
     uri = uri.replace('postgres://', 'postgresql://', 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
 db.init_app(app)
-
 
 app.register_blueprint(main)
 
+
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
